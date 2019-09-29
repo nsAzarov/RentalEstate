41c72e2d97a69ba50aef0d083c95414822e32cfd
nsAzarov <nsazarov99@gmail.com>
2019-09-29 15:52:54 +0300

    set all needed styles for Item component

--- client/src/Components/Home/Item.js
+++ client/src/Components/Home/Item.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
+import bathtub from '../../png/bathtub.png';
+import bed from '../../png/bed.png';
+import group from '../../png/group.png';
 
 const ItemBlock = styled.div`
 	height: 380px;
@@ -42,6 +45,33 @@ const Description = styled.h2`
     -webkit-box-orient: vertical;
 `;
 
+const Info = styled.div`
+	height: 40px;
+	width: 100%;
+	margin-top: 10px;
+	display: flex;
+	align-items: center;
+	position: relative;
+	.small-info {
+		margin: 0 10px;
+		display: flex;
+		align-items: center;
+		font-size: 16px;
+		font-weight: bold;
+		img {
+			height: 30px;
+			margin-right: 5px;
+		}
+	}
+	#price {
+		position: absolute;
+		right: 10px;
+		font-size: 28px;
+		font-weight: bold;
+		color: #f0cb86;
+	}
+`;
+
 const Item = (props) => {
 	let img = require(`../../png/${props.item.images[0]}`);
 	return (
@@ -49,6 +79,21 @@ const Item = (props) => {
 			<ImageSection backgroundImage={img} alt=""/>
 			<Title>{props.item.heading}</Title>
 			<Description>{props.item.description}</Description>
+			<Info>
+				<div className="small-info">
+					<img src={bed} alt=""/>
+					{props.item.bedrooms}
+				</div>
+				<div className="small-info">
+					<img src={bathtub} alt=""/>
+					{props.item.bathrooms}
+				</div>
+				<div className="small-info">
+					<img src={group} alt=""/>
+					{props.item.guests}
+				</div>
+				<div id="price">53 268 $/n</div>
+			</Info>
 		</ItemBlock>
 	);
 }
