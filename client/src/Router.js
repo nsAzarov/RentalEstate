74ae98bb51b8eb2a41d72e8a5a8a27905780e88b
nsAzarov <nsazarov99@gmail.com>
2019-10-08 20:45:08 +0300

    add scrolling to top after redirect

--- client/src/Router.js
+++ client/src/Router.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {BrowserRouter, Route} from "react-router-dom";
 import Home from './Components/Home';
 import Apartment from './Components/Apartment';
+import ScrollToTop from './Components/Master/ScrollToTop';
 
 const Router = () => {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={Home} />
-            <Route path="/Apartment/:id" component={Apartment} />
+            <ScrollToTop>
+                <Route path="/" exact component={Home} />
+                <Route path="/Apartment/:id" component={Apartment} />
+            </ScrollToTop>
         </BrowserRouter>
     )
 }
